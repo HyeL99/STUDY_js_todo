19c979bae4863bc6532a484e5d8a919574495228
HyeL99 <kanghr8149@gmail.com>
2022-04-22 00:46:46 +0900

    second commit

--- main.js
+++ main.js
@@ -11,10 +11,24 @@
 
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
-let taskList = []
+let tabs = document.querySelectorAll(".task-taps div");
+let underLine = document.getElementById("under-line");
+let taskList = [];
+let filterList = [];
+let mode = "all";
 
-addButton.addEventListener("click", addTask)
-console.log(taskInput)
+addButton.addEventListener("click", addTask);
+/*
+underLine.style.width = offsetWidth + "px";
+underLine.style.left = offsetLeft + "px";
+underLine.style.top = offsetTop + (e.target.offsetHeight - 4) + "px";
+*/
+for(let i=1; i<tabs.length; i++){
+    tabs[i].addEventListener("click", function (event){filter(event);});
+}
+function enterkey(){
+    if(window.event.keyCode == 13){addTask();}
+}
 
 function addTask(){
     let task = {
@@ -23,29 +37,37 @@ function addTask(){
         isComplete: false
     };
     taskList.push(task);
-    console.log(taskList);
     render();
+    taskInput.value = "";
 }
 
 function render(){
+    let list = [];
+    if(mode == "all"){
+        list = taskList;
+    }else{
+        list = filterList;
+    }
+    //console.log(list)
+
     let resultHTML = "";
-    for(let i=0; i<taskList.length; i++){
-        if(taskList[i].isComplete == true){
+    for(let i=0; i<list.length; i++){
+        if(list[i].isComplete == true){
             resultHTML += `<div class="done-area">
             <div class="task">
-                <div class="task-done">${taskList[i].taskContent}</div>
-                <div>
-                    <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-                    <button onclick="delateTask('${taskList[i].id}')">Delate</button>
+                <div class="task-done task-text">${list[i].taskContent}</div>
+                <div class="task-button">
+                    <button onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-arrow-rotate-right icon-repeat"></i></button>
+                    <button onclick="delateTask('${list[i].id}')"><i class="fa-regular fa-trash-can icon-delate"></i></button>
                 </div>
             </div>
             </div>`;
         } else {
             resultHTML += `<div class="task">
-                <div>${taskList[i].taskContent}</div>
-                <div>
-                    <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-                    <button onclick="delateTask('${taskList[i].id}')">Delate</button>
+                <div class="task-text">${list[i].taskContent}</div>
+                <div class="task-button">
+                    <button onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-check icon-check"></i></button>
+                    <button onclick="delateTask('${list[i].id}')"><i class="fa-regular fa-trash-can icon-delate"></i></button>
                 </div>
             </div>`;
         }
@@ -64,8 +86,7 @@ function toggleComplete(id){
             break;
         }
     }
-    render();
-    console.log(taskList)
+    filter();
 }
 
 function delateTask(id){
@@ -73,6 +94,29 @@ function delateTask(id){
         if(taskList[i].id==id){
             taskList.splice(i, 1);
             break;
+        }
+    }
+    filter();
+}
+function filter(event){
+    if(event){
+        mode = event.target.id;
+        underLine.style.width = event.target.offsetWidth + "px";
+        underLine.style.left = event.target.offsetLeft + "px";
+        underLine.style.top = event.target.offsetTop + (event.target.offsetHeight - 4) + "px";
+    }
+    filterList = [];
+    if(mode == "notDone"){
+        for(let i=0; i<taskList.length; i++){
+            if(taskList[i].isComplete == false){
+                filterList.push(taskList[i]);
+            }
+        }
+    }else if(mode == "done"){
+        for(let i=0; i<taskList.length; i++){
+            if(taskList[i].isComplete == true){
+                filterList.push(taskList[i]);
+            }
         }
     }
     render();
